1e70108530e2caaf39b06d025b60f764f9544ac1
fahrur <fahrurben@gmail.com>
2025-03-01 06:50:36 +0700

    Add lint file

--- kakeibo/models/custom_user.py
+++ kakeibo/models/custom_user.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext as _
 
+
 class CustomUser(AbstractUser):
     email = models.EmailField(_('email address'), unique=True)
     birthday = models.DateField(_('birthday'))
--- kakeibosite/settings.py
+++ kakeibosite/settings.py
@@ -10,7 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+from datetime import timedelta
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*mf2v1286ze^+w2(duwuxa@*)1ue_@kg_13j7=&9rax8br^9e8'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG_MODE")
 
 ALLOWED_HOSTS = []
 
