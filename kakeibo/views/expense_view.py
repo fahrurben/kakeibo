7268a4f7bfce5c31557016752dc9aa8b2721175c
fahrur <fahrurben@gmail.com>
2025-03-23 14:59:18 +0700

    Income and Expense total

--- kakeibo/views/expense_view.py
+++ kakeibo/views/expense_view.py
@@ -1,9 +1,11 @@
 from rest_framework import viewsets
+from rest_framework.views import Response, status
+from rest_framework.decorators import action
 
 from ..models.expense import Expense
 from ..serializers.expense_serializer import ExpenseSerializer
 from ..permissions.user_owned_permission import UserOwnedPermission
-
+from django.db.models import Sum
 
 class ExpenseView(viewsets.ModelViewSet):
     serializer_class = ExpenseSerializer
@@ -27,3 +29,17 @@ class ExpenseView(viewsets.ModelViewSet):
                                  date__year__lte=year, date__month__lte=month)
 
         return query
+
+    @action(detail=False)
+    def monthly_total(self, request):
+        user = self.request.user
+        month = self.request.query_params.get('month')
+        year = self.request.query_params.get('year')
+
+        query = Expense.objects.filter(user=user)
+        if month is not None and year is not None:
+            query = query.filter(date__year__gte=year, date__month__gte=month,
+                                 date__year__lte=year, date__month__lte=month)
+
+        ret = query.aggregate(Sum('amount'))
+        return Response(ret, status=status.HTTP_200_OK)
--- kakeibo/views/income_view.py
+++ kakeibo/views/income_view.py
@@ -1,8 +1,11 @@
 from rest_framework import viewsets
+from rest_framework.views import Response, status
 
+from rest_framework.decorators import action
 from ..models.income import Income
 from ..serializers.income_serializer import IncomeSerializer
 from ..permissions.user_owned_permission import UserOwnedPermission
+from django.db.models import Sum
 
 
 class IncomeView(viewsets.ModelViewSet):
@@ -27,3 +30,17 @@ class IncomeView(viewsets.ModelViewSet):
                                  date__year__lte=year, date__month__lte=month)
 
         return query
+
+    @action(detail=False)
+    def monthly_total(self, request):
+        user = self.request.user
+        month = self.request.query_params.get('month')
+        year = self.request.query_params.get('year')
+
+        query = Income.objects.filter(user=user)
+        if month is not None and year is not None:
+            query = query.filter(date__year__gte=year, date__month__gte=month,
+                                 date__year__lte=year, date__month__lte=month)
+
+        ret = query.aggregate(Sum('amount'))
+        return Response(ret, status=status.HTTP_200_OK)
